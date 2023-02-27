9bf4fa0e5dad7d57403385f3e57709e7a5f3abf9
pallavi <pallavi20raj@gmail.com>
2023-02-27 10:13:50 +0530

    Initial commit

---
+++ app/src/main/java/com/pallavi/dialog_task/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.pallavi.dialog_task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+   lateinit var fab:floatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
